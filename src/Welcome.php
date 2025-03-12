82765cf9931b9556dd5ed8e39bfc8d91f969b68f
Wendy <wendyhdiyono@gmail.com>
2025-03-12 21:32:41 +0700

    Constructor removed

--- src/Welcome.php
+++ src/Welcome.php
@@ -3,11 +3,9 @@
 namespace WendyHadiyono\Belajar;
 
 class Welcome {
-    public function __construct(private string $name) {
-        
-    }
+    private $name;
 
     public function sayHello(string $name = "Guest"): string {
-        return "Hello $name, my name is $this->name";
+        return "Hello world! My name is $this->name!";
     }
 }
